85c30329ca7c99508dcf7c49209014f3674cf745
GN Trifanoff <aznaum@gmail.com>
2013-05-03 15:27:17 -0700

    segregate public web files into dedicated subfolder

---
+++ web/jove.cgi
@@ -0,0 +1,8 @@
+#!/Users/george/dart2/dart-sdk/bin/dart
+
+import 'dart:io';
+
+main() {
+  print('Content-type: text/html\n\n');
+  print('by jove!');
+}
---
+++ web/jove.dart
@@ -0,0 +1,10 @@
+#!/Users/george/dart2/dart-sdk/bin/dart
+
+import 'dart:io';
+
+main() {
+  print('Content-type: text/html\n\n');
+  print('by jove!');
+  print('<br><br>');
+  print(Platform.environment);
+}
