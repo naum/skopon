15bdca87f9324caa036c4704fe389eed610d897e
GN Trifanoff <aznaum@gmail.com>
2013-05-03 16:33:28 -0700

    RegExp template tinkering

--- web/jove.dart
+++ web/jove.dart
@@ -2,17 +2,46 @@
 
 import 'dart:io';
 
+var template = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>skopon</title>
+<link rel="stylesheet" href="/style/main.css">
+</head>
+<body>
+<h1>{{PAGEHEADER}}</h1>
+{{PAGEBODY}}
+<footer>
+{{PAGEFOOTER}}
+</footer>
+</body>
+</html>
+''';
+
 main() {
   outputHeaders();
   displayEnvironment();
 }
 
 displayEnvironment() {
+  var sb = new StringBuffer();
   var sk = Platform.environment.keys.toList();
   sk.sort();
   for (var k in sk) {
-      print('$k: ${Platform.environment[k]}<br>\n');
+      sb.write('$k: ${Platform.environment[k]}<br>\n');
   };
+  var pagebind = {
+    'PAGEHEADER': 'Platform.environment',
+    'PAGEBODY': sb.toString()
+  };
+  var reTS = new RegExp(r'\{\{(\w+)\}\}');
+  var pbody = template.replaceAllMapped(reTS, (m) {
+    var tv = m.group(1);
+    return (pagebind.containsKey(tv)) ? pagebind[tv] : '';
+  }); 
+  print(pbody);
 }
 
 outputHeaders() {
